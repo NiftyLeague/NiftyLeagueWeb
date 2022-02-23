2dc79d659aba9fe9cd788f561a0ba73e122b0f02
NiftyMichael <brandon@niftyleague.com>
2022-02-22 23:16:52 -0500

    factor out degens and sponsors

---
+++ components/carousel/constants.tsx
@@ -0,0 +1,40 @@
+export const SponsorImageURLs = [
+    "/img/spons2.png",
+    "/img/spons3.png",
+    "/img/spons4.png",
+    "/img/spons5.png",
+    "/img/spons6.png",
+]
+
+export const DegenData = [
+    {
+        name: "Cribbit",
+        createdDate: "9/22/2021 2:49 PM",
+        source: "/img/cribbit.png",
+    },
+    {
+        name: "Sonic",
+        createdDate: "9/22/2021 2:49 PM",
+        source: "/img/sonic.png",
+    },
+    {
+        name: "King Kong",
+        createdDate: "9/22/2021 2:49 PM",
+        source: "/img/kingkong.png",
+    },
+    {
+        name: "The Grim Reaper",
+        createdDate: "9/22/2021 2:49 PM",
+        source: "/img/grimReaper.png",
+    }, 
+    {
+        name: "GRAPE",
+        createdDate: "9/22/2021 2:49 PM",
+        source: "/img/grapes.png",
+    },
+    {
+        name: "Patches",
+        createdDate: "9/22/2021 2:49 PM",
+        source: "/img/patches.png",
+    }, 
+]
