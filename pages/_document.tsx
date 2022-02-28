576e148b8994f8a8136fe2f35a000499fbfb43d9
NiftyAndy <andy@niftyleague.com>
2022-02-28 12:19:43 -0800

    Add microsoft clarity

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,6 +17,17 @@ export default function Document() {
           `,
           }}
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_TAG}");
+          `,
+          }}
+        />
         <link rel="icon" href="/favicon.png" type="image/x-icon" />
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" />
         <link
