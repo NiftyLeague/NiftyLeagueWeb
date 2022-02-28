9372c5b0fd0f9a1e0ca0b936ec871ba39dec171f
NiftyAndy <andy@niftyleague.com>
2022-02-28 12:12:09 -0800

    config google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,10 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
+import * as ga from '../lib/ga';
+
 import '../styles/app.css';
 import '../styles/slick.css';
 import '../styles/roadmap.css';
@@ -6,10 +13,24 @@ import '../styles/blog.scss';
 import '../styles/footer.scss';
 import '../styles/header.scss';
 
-import type { AppProps } from 'next/app';
-import Head from 'next/head';
-
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      ga.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Head>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,19 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
         <link rel="icon" href="/favicon.png" type="image/x-icon" />
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" />
         <link
