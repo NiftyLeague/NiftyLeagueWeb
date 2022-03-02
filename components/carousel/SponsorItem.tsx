8ebcb7e022bdfadd0bf1e6e6be5b8191165445b3
NiftyAndy <andy@niftyleague.com>
2022-03-02 01:13:06 -0800

    Add back sponsors section

--- components/carousel/SponsorItem.tsx
+++ components/carousel/SponsorItem.tsx
@@ -12,11 +12,9 @@ const SponsorCarouselItem = ({
   height: number;
 }): JSX.Element => (
   <a href={url} target="_blank" rel="noreferrer">
-    <div className="slide">
-      <div className="slide-content">
-        <div className="spons-single">
-          <Image alt="sponsor image" src={source} layout="responsive" width={width} height={height} />
-        </div>
+    <div className="slide-content">
+      <div className="spons-single">
+        <Image alt="sponsor image" src={source} layout="responsive" width={width} height={height} priority />
       </div>
     </div>
   </a>
