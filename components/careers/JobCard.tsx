6756a6a48879222372d917f35c37eed2b2474585
crystal-clear-moon <bright_96_628@outlook.com>
2022-04-28 23:31:45 -0400

    fix style based in mockup

--- components/careers/JobCard.tsx
+++ components/careers/JobCard.tsx
@@ -21,7 +21,7 @@ const JobCard = ({ details }: JobCardProps): JSX.Element => {
   const [open, setOpen] = useState(false);
   return (
     <MDBCard background='dark' className="m-3 job-card my-5">
-      <div className="card-header position-relative p-5 cursor-pointer jobs-card-header border-bottom-0" onClick={() => setOpen(!open)}>
+      <div className="card-header position-relative p-4 cursor-pointer jobs-card-header border-bottom-0" onClick={() => setOpen(!open)}>
         <div>
           <h3>{title}</h3>
           <span>{location}</span>
@@ -34,27 +34,27 @@ const JobCard = ({ details }: JobCardProps): JSX.Element => {
         </div>
       </div>
 
-      <MDBCollapse show={open} className='pt-0 p-4 h-auto'>
-        <h4 className="color-purple">About the Role</h4>
-        <p>&nbsp;&nbsp;{aboutTheRole}</p>
+      <MDBCollapse show={open} className='p-4 h-auto'>
+        <h4 className="color-purple md-text-left">About the Role</h4>
+        <p className="md-text-left">{aboutTheRole}</p>
         {
           responsibilities &&
           <p>
-            <h4 className="color-purple pt-5">Responsibilities:</h4>
-            <ul>
+            <h4 className="color-purple pt-5 md-text-left">Responsibilities:</h4>
+            <ul className="md-text-left">
               {responsibilities.map((resp, index) => <li key={index}>{resp}</li>)}
             </ul>
           </p>
         }
         <p>
-          <h4 className="color-purple pt-5">Requirements:</h4>
-          <ul>
+          <h4 className="color-purple pt-5 md-text-left">Requirements:</h4>
+          <ul className="md-text-left">
             {requirements.map((req, index) => <li key={index}>{req}</li>)}
           </ul>
         </p>
         <p>
-          <h4 className="color-purple pt-5">What we offer:</h4>
-          <ul>
+          <h4 className="color-purple pt-5 md-text-left">What we offer:</h4>
+          <ul className="md-text-left">
             {benefits.map((ben, index) => <li key={index}>{ben}</li>)}
           </ul>
         </p>
