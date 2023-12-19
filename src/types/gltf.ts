caa6a72cf8f681044774c9a861941ad001eb18dc
NiftyAndy <83057795+NiftyAndy@users.noreply.github.com>
2023-12-19 00:03:13 -0400

    Reapply 3D Degens code with fix (#211)

---
+++ src/types/gltf.ts
@@ -0,0 +1,21 @@
+export enum SRC {
+  IMAGE = '2D',
+  MODEL = 'Box',
+  SPRITE = 'Sprite',
+}
+
+export type Color =
+  | 'salmon'
+  | 'purple'
+  | 'blue'
+  | 'bluegrey'
+  | 'bluepurple'
+  | 'green'
+  | 'bluegreen'
+  | 'brown'
+  | 'ochre'
+  | 'palepink'
+  | 'yellow'
+  | 'greenish'
+  | 'lightblue'
+  | 'ochretwo';
