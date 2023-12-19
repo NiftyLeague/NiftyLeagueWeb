0f6f0fe516d4446766202a9ae8563c62f828c313
NiftyAndy <83057795+NiftyAndy@users.noreply.github.com>
2023-12-18 22:58:34 -0400

    Revert "3D Degens base code and cleanup gltf view (#209)" (#210)

--- src/lib/numbers.ts
+++
@@ -1,7 +0,0 @@
-export const formatNumberToDisplay = (num: number, digits = 2) =>
-  num
-    ? num.toLocaleString(undefined, {
-        minimumFractionDigits: digits,
-        maximumFractionDigits: digits,
-      })
-    : 0;
