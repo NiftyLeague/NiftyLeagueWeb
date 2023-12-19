aec59b639608db99253cd1ff15ba1e589d4f7a07
NiftyAndy <andy@niftyleague.com>
2023-12-19 02:35:31 -0400

    test with viem client

--- src/hooks/useClaimableNFTL.ts
+++ src/hooks/useClaimableNFTL.ts
@@ -1,6 +1,13 @@
-import { useContractRead } from 'wagmi';
-import { formatEther } from 'viem';
+import { useEffect, useState } from 'react';
+// import { useContractRead } from 'wagmi';
+import { formatEther, createPublicClient, http, Abi } from 'viem';
 import CONTRACTS from '@/constants/contracts';
+import { mainnet } from 'viem/chains';
+
+export const publicClient = createPublicClient({
+  chain: mainnet,
+  transport: http(),
+});
 
 const NFTL_CONTRACT = CONTRACTS.NFTLToken;
 
@@ -10,15 +17,32 @@ interface NFTLClaimableState {
 }
 
 export default function useClaimableNFTL(tokenId: number | string): NFTLClaimableState {
-  const { data, isLoading: loading } = useContractRead({
-    address: NFTL_CONTRACT.address,
-    abi: NFTL_CONTRACT.abi,
-    functionName: 'accumulated',
-    args: [tokenId],
-    cacheTime: 300_000,
-    enabled: !!tokenId,
-    select: (data: unknown) => (data ? parseFloat(formatEther(data as bigint)) : 0),
-  });
+  // const { data, isLoading: loading } = useContractRead({
+  //   address: NFTL_CONTRACT.address,
+  //   abi: NFTL_CONTRACT.abi,
+  //   functionName: 'accumulated',
+  //   args: [tokenId],
+  //   cacheTime: 300_000,
+  //   enabled: !!tokenId,
+  //   select: (data: unknown) => (data ? parseFloat(formatEther(data as bigint)) : 0),
+  // });
 
-  return { totalAccrued: data || 0, loading };
+  // return { totalAccrued: data || 0, loading };
+
+  const [totalAccrued, setTotalAccrued] = useState(0);
+
+  useEffect(() => {
+    const readContract = async () => {
+      const data = await publicClient.readContract({
+        address: NFTL_CONTRACT.address,
+        abi: NFTL_CONTRACT.abi as Abi,
+        functionName: 'accumulated',
+        args: [tokenId],
+      });
+      setTotalAccrued(data ? parseFloat(formatEther(data as bigint)) : 0);
+    };
+    if (tokenId) readContract();
+  }, [tokenId]);
+
+  return { totalAccrued, loading: false };
 }
